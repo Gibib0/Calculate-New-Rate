e7005e9e090077f847d2725f1d4d9e9ccd621093
Anastasia <anast111antonova@gmail.com>
2025-08-25 14:03:45 +0300

    Add checks for negative number and data type

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 'use strict'
 
+function calculateRating(winRating, loseRating) {
+	if(winRating < 0 || loseRating < 0) {
+		return null
+	}
+
+	if(typeof winRating !== 'number' || typeof loseRating !== 'number') {
+		return null
+	}
+}
