498b768e058dd0cac8469571b1ef020954e6674f
Anastasia <anast111antonova@gmail.com>
2025-08-25 14:43:08 +0300

    Change names; add calculation of the number of points for different differences

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 'use strict'
 
-function calculateRating(winRating, loseRating) {
-	if(winRating < 0 || loseRating < 0) {
+function calculateRating(myRating, opponentRating) {
+	if(myRating < 0 || opponentRating < 0) {
 		return null
 	}
 
-	if(typeof winRating !== 'number' || typeof loseRating !== 'number') {
+	if(typeof myRating !== 'number' || typeof opponentRating !== 'number') {
 		return null
 	}
+
+	if(myRating === 0) {
+		return opponentRating
+	}
+	if(opponentRating === 0) {
+		return myRating
+	}
+
+	const difference = myRating - opponentRating
+
+	if(difference >= 0 && difference <= 2) {
+		myRating += 2
+	} else if(difference > 2 && difference < 20) {
+		myRating += 1
+	} else if(difference < 0) {
+		myRating += (opponentRating - myRating + 5) / 3
+	}
+
+	
 }
